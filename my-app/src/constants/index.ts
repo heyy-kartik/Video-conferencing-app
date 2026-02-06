f12cf233949aeefeae4f982c3ff2a156791170c9
heyy-kartik <kartikjagdale0511@gmail.com>
2026-02-06 19:07:04 +0530

    Add layout and pages for different ui

---
+++ my-app/src/constants/index.ts
@@ -0,0 +1,27 @@
+const sidebarlinks = [
+  {
+    label : "Home",
+    imgurl : " ", 
+    route : "/" , 
+    },
+     {
+    label : "Upcoming Meetings",
+    imgurl : " ", 
+    route : "upcoming" , 
+    },
+     {
+    label : "Previous",
+    imgurl : " ", 
+    route : "previous" , 
+    },
+     {
+    label : "Recordings",
+    imgurl : " ", 
+    route : "recordings" , 
+    },
+     {
+    label : "Personal Room",
+    imgurl : " ", 
+    route : "personal-room" , 
+    }
+] 
