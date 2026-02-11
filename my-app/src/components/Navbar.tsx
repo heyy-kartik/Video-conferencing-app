76e19eae22e9c682fa8ad73acf8f6cf0a668b6b7
heyy-kartik <kartikjagdale0511@gmail.com>
2026-02-11 19:30:39 +0530

    refactor: clean up Navbar component and improve Sidebar layout; adjust styles and structure

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="text-2xl  pl-5 pt-4 text-blue-400">Navbar</div>;
+  return <div className="text-2xl  pl-5 pt-4 text-blue-400"></div>;
 };
 
 export default Navbar;
