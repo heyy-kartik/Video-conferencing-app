2e19c39db27e5816f4d37522c6af3a0b702c0812
heyy-kartik <kartikjagdale0511@gmail.com>
2026-02-12 02:46:57 +0530

    nothing just chatgpt it bro

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -5,20 +5,20 @@ import Mobilenav from "./Mobilenav";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 const Navbar = () => {
   return (
-    <nav className="flex-between  fixed z-50 w-full px-6  py-4 lg:px-10">
+    <nav className="flex justify-between items-center fixed z-50 w-full bg-(--dark-2) px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image src="/icons/logo.svg" alt="hamburger" width={32} height={32} />
         <p className="text-[26px] font-extrabold text-white max-sm:hidden">
-          Zoom{" "}
+          Zoom
         </p>
       </Link>
 
-      <div className="flex-between gap-5">
+      <div className="flex items-center gap-5">
         <SignedIn>
           <UserButton afterSignOutUrl="/sign-in" />
         </SignedIn>
+        <Mobilenav />
       </div>
-      <Mobilenav />
     </nav>
   );
 };
