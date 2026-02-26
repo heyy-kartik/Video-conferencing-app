491cb61114499fca9aad12b3e5231f71e9d0327e
heyy-kartik <kartikjagdale0511@gmail.com>
2026-02-27 03:15:21 +0530

    feat: rename page component to Meeting; log meeting ID and update rendering logic

--- my-app/src/app/(root)/meeting/[id]/page.tsx
+++ my-app/src/app/(root)/meeting/[id]/page.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @next/next/no-async-client-component */
 "use client";
 import React from "react";
 import { useParams } from "next/navigation";
 
-const page = ({ params }: { params: { id: string } }) => {
-  return <div>Meeting Room :{params.id} </div>;
+const Meeting = ({ params }: { params: { id: string } }) => {
+  const { id } = React.use(params);
+  console.log("Meeting ID:", id);
+  return <div>Meeting Room :{id} </div>;
 };
 
-export default page;
+export default Meeting;
