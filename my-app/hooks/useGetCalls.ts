7c4c745b74f20c74eeecd9359a6c0cad05c367da
heyy-kartik <kartikjagdale0511@gmail.com>
2026-03-03 00:59:26 +0530

    staying on streak

--- my-app/hooks/useGetCalls.ts
+++ my-app/hooks/useGetCalls.ts
@@ -6,11 +6,44 @@ import { useEffect , useState } from 'react'
 const useGetCalls = () => {
 
      
-     
   const [calls, setcalls] = useState<Call[]>([])
   const [isLoading, setisLoading] = useState(false)
   const client = useStreamVideoClient(); 
   const {user} = useUser();
+
+   useEffect(() => {
+      
+    const loadCalls = async () => {
+      if(!client || !user?.id) return;
+      setisLoading(true);
+      try {
+        const {calls } = await client.queryCalls({
+          sort: [{field: 'starts_at', direction: -1}],
+           filter_conditions : {
+            starts_at : {$exists : true}, 
+            $or : [
+              {created_by_id : user.id}, 
+              {participants : {$in : [user.id]}}
+            ], 
+           }, 
+        }); 
+        setcalls(calls);
+      } catch (error) {
+        console.error("Error fetching calls:", error);
+      }
+       finally {setisLoading(false) }
+    }  
+    loadCalls();}, 
+      
+              
+     
+       [client , user?.id])
+     
 }
 
+const now = new Date(); 
+
+  const endedCalls = Call?.filter(({ state: { startsAt, endedAt } }: Call) => {
+    return (startsAt && new Date(startsAt) < now) || !!endedAt
+  })
 export default useGetCalls; 
