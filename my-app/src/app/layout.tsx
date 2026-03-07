2e65dc58fec1f11a92f8804f9a13aafbdfef48d0
heyy-kartik <kartikjagdale0511@gmail.com>
2026-03-07 19:09:19 +0530

    END2

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Zoom",
+  title: "EZMeet",
   description: "Video Conferencing Application Using WebRTC & Next.js",
 };
 
